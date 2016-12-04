8e1ca1f7a0a2cd97861d7c4c8003c8cf10a71a0c
shuangchan <shuangchan@berkeley.edu>
2016-12-04 16:10:05 -0500

    Add files via upload

--- schema/twitter_data_schema_revised.sql
+++ schema/twitter_data_schema_revised.sql
@@ -1,54 +1,21 @@
 
---Create Twitter Raw JSON table
+--Twitter Sentiment Analysis Schema
+--UCB W205 Course Project
+--Created By: Shuang Chan, David Skarbrevik
+--Created On: 11/01/2016
+--Target Environment: HIVE, SPARK
+
+set hive.exec.dynamic.partition = true;
+set hive.exec.dynamic.partition.mode=nonstrict;
+set hive.mapred.supports.subdirectories=true;
+set mapred.input.dir.recursive=true;
+
+--Table to store the raw Twitter JSON data
 drop table IF EXISTS twitter_raw_json;
-create table twitter_raw_json
+create EXTERNAL table twitter_raw_json
 (
         topic string,
         json_response string
-)
-partitioned by (batchid int)
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde';
---ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' ESCAPED BY '\\';
-
-
---Create Twitter Structured Data table
-drop table IF EXISTS twitter_structured_data;
-create table twitter_structured_data
-(
-        topic string,
-        id bigint,
-        created_at_dt timestamp,
-        in_reply_to_user_id_str string,
-        text string,
-        contributors string,
-        retweeted string,
-        truncated string,
-        coordinates string,
-        source string,
-        retweet_count int,
-        first_hashtag string,
-        first_user_mention string,
-        screen_name string,
-        name string,
-        followers_count int,
-        listed_count int,
-        friends_count int,
-        lang string,
-        user_location string,
-        time_zone string
-)
-partitioned by (batchid int);
-
-CREATE INDEX idx_created_at_dt_01 ON TABLE twitter_structured_data (created_at_dt, topic) AS 'COMPACT' WITH DEFERRED REBUILD;
-
-
-drop table IF EXISTS twitter_result;
-CREATE EXTERNAL TABLE twitter_result
-(
-topic string,
-sentiment string,
-created_at_dt timestamp,
-batchid int
 )
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
@@ -57,6 +24,29 @@ WITH SERDEPROPERTIES (
 "escapeChar" = '\\'
 )
 STORED AS TEXTFILE
+LOCATION '/user/w205/twitter/twitter-topic/';
+
+--Table to store the results of the sentiment analysis
+drop table IF EXISTS twitter_result;
+CREATE EXTERNAL TABLE twitter_result
+(
+        topic string,
+        sentiment string,
+        created_at_dt timestamp,
+        valid_words string
+)
+partitioned by (xdate date)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\'
+)
+STORED AS TEXTFILE
 LOCATION '/user/w205/twitter/twitter_result';
+
+
+
+
 
 
